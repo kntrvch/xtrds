67ff060397399746a9a845821247f712a4a08654
JK <kntrvch@gmail.com>
2023-07-21 15:41:56 +0200

    adjust break points

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -13,13 +14,11 @@ export class AppComponent {
 
   ngOnInit() {
     this.breakpointObserver
-      .observe(['(min-width: 500px)'])
+      .observe(['(min-width: 769px)'])
       .subscribe((state: BreakpointState) => {
         if (state.matches) {
-          console.log('Viewport width is 500px or greater!');
           this.mobileView = false;
         } else {
-          console.log('Viewport width is less than 500px!');
           this.mobileView = true;
         }
       });
